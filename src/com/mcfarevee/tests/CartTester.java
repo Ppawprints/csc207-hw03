48cf35f6f75fc1bbdda86b8a4c310d1fb197dd58
Ppawprints <dongyila@grinnell.edu>
2019-02-14 20:42:00 -0600

    FInal CaRT TESter

--- src/com/mcfarevee/tests/CartTester.java
+++ src/com/mcfarevee/tests/CartTester.java
@@ -11,90 +11,89 @@ import com.mcfarevee.groceries.Weight;
 import com.mcfarevee.shopping.Cart;
 
 public class CartTester {
-    public static void main(String[] args) {
-        /* Instanize object for testing */
-        /* from https://www.cs.grinnell.edu/~rebelsky/Courses/CSC207/2019S/01/
-                assignments/assignment03.html */
-        Cart cart = new Cart();
-        PrintWriter pen = new PrintWriter(System.out, true);
+  public static void main(String[] args) {
+    /* Instanize object for testing */
+    /*
+     * from https://www.cs.grinnell.edu/~rebelsky/Courses/CSC207/2019S/01/
+     * assignments/assignment03.html
+     */
+    Cart cart = new Cart();
+    PrintWriter pen = new PrintWriter(System.out, true);
 
-        BulkFood bananas = new BulkFood("bananas", Unit.POUND, 50, 20);
-        BulkFood saffron = new BulkFood("saffron", Unit.GRAM, 1000, 200);
+    BulkFood bananas = new BulkFood("bananas", Unit.POUND, 50, 20);
+    BulkFood saffron = new BulkFood("saffron", Unit.GRAM, 1000, 200);
 
-        BulkItem bulkItemBananas = new BulkItem(bananas, Unit.POUND, 3);
-        BulkItem bulkItemSaffron = new BulkItem(saffron, Unit.GRAM, 1);
-        Package packageAmazon = new Package("amazon", 
-                                            new Weight(Unit.GRAM, 100),
-                                            100);
-        ManyPackages manyPackageAmazon = new ManyPackages(new Package("amazon",
-                                                 new Weight(Unit.GRAM, 100),
-                                                 100), 5);
-        NonFood nonFoodStone = new NonFood("stone", new Weight(Unit.POUND, 200),
-                                           99);
-        
-        /* Add them into Cart */
-        cart.addItem(bulkItemBananas);
-        cart.addItem(bulkItemSaffron);
-        cart.addItem(packageAmazon);
-        cart.addItem(manyPackageAmazon);
-        cart.addItem(nonFoodStone);
+    BulkItem bulkItemBananas = new BulkItem(bananas, Unit.POUND, 3);
+    BulkItem bulkItemSaffron = new BulkItem(saffron, Unit.GRAM, 1);
+    Package packageAmazon = new Package("amazon", new Weight(Unit.GRAM, 100), 100);
+    ManyPackages manyPackageAmazon =
+        new ManyPackages(new Package("amazon", new Weight(Unit.GRAM, 100), 100), 5);
+    NonFood nonFoodStone = new NonFood("stone", new Weight(Unit.POUND, 200), 99);
 
-        /* Test function of each class */
+    /* Add them into Cart */
+    cart.addItem(bulkItemBananas);
+    cart.addItem(bulkItemSaffron);
+    cart.addItem(packageAmazon);
+    cart.addItem(manyPackageAmazon);
+    cart.addItem(nonFoodStone);
 
-        /* BulkItem */
-        pen.println("BulkItem Test");
-        pen.println("toString(): " + bulkItemBananas.toString());
-        pen.println("getWeight(): " + bulkItemBananas.getWeight().unit.plural());
-        pen.println("getPrice(): " + bulkItemBananas.getPrice());
-        pen.println("getName(): " + bulkItemBananas.getName());
-        pen.println("equals(): " + bulkItemBananas.equals(saffron)); // should be false
-        pen.println();
+    /* Test function of each class */
 
-        /* ManyPackages */
-        pen.println("ManyPackages Test");
-        pen.println("toString(): " + manyPackageAmazon.toString());
-        pen.println("getWeight(): " + manyPackageAmazon.getWeight().unit.plural());
-        pen.println("getPrice(): " + manyPackageAmazon.getPrice());
-        pen.println("getName(): " + manyPackageAmazon.getName());
-        pen.println("equals(): " + manyPackageAmazon.equals(manyPackageAmazon)); 
-                                // should be true
-        pen.println();
+    /* BulkItem */
+    pen.println("BulkItem Test");
+    pen.println("toString(): " + bulkItemBananas.toString());
+    pen.println("getWeight(): " + bulkItemBananas.getWeight().unit.plural());
+    pen.println("getPrice(): " + bulkItemBananas.getPrice());
+    pen.println("getName(): " + bulkItemBananas.getName());
+    pen.println("equals(): " + bulkItemBananas.equals(bulkItemSaffron));
+    // should be false
+    pen.println();
 
-        /* NonFood */
-        pen.println("NonFood Test");
-        pen.println("toString(): " + nonFoodStone.toString());
-        pen.println("getWeight(): " + nonFoodStone.getWeight().unit.plural());
-        pen.println("getPrice(): " + nonFoodStone.getPrice());
-        pen.println("getName(): " + nonFoodStone.getName());
-        pen.println("equals(): " + nonFoodStone.equals(new NonFood("stone", new Weight(Unit.POUND, 200), 99)));
-                                    // should be true
-        pen.println();
+    /* ManyPackages */
+    pen.println("ManyPackages Test");
+    pen.println("toString(): " + manyPackageAmazon.toString());
+    pen.println("getWeight(): " + manyPackageAmazon.getWeight().unit.plural());
+    pen.println("getPrice(): " + manyPackageAmazon.getPrice());
+    pen.println("getName(): " + manyPackageAmazon.getName());
+    pen.println("equals(): " + manyPackageAmazon.equals(manyPackageAmazon));
+    // should be true
+    pen.println();
 
-        /* Package */
-        pen.println("Package Test");
-        pen.println("toString(): " + packageAmazon.toString());
-        pen.println("getWeight(): " + packageAmazon.getWeight().unit.plural());
-        pen.println("getPrice(): " + packageAmazon.getPrice());
-        pen.println("getName(): " + packageAmazon.getName());
-        pen.println("equals(): " + packageAmazon.equals(packageAmazon));
-                                    // should be true
-        pen.println();
+    /* NonFood */
+    pen.println("NonFood Test");
+    pen.println("toString(): " + nonFoodStone.toString());
+    pen.println("getWeight(): " + nonFoodStone.getWeight().unit.plural());
+    pen.println("getPrice(): " + nonFoodStone.getPrice());
+    pen.println("getName(): " + nonFoodStone.getName());
+    pen.println(
+        "equals(): " + nonFoodStone.equals(new NonFood("stone", new Weight(Unit.POUND, 200), 99)));
+    // should be true
+    pen.println();
 
-        /* Cart Test */
-        pen.println("numThings(): " + cart.numThings());
-        pen.println("numEntries(): " + cart.numEntries());
-        pen.println("Content in the cart");
-        cart.printContent(pen);
-        pen.println("getPrice(): " + cart.getPrice());
+    /* Package */
+    pen.println("Package Test");
+    pen.println("toString(): " + packageAmazon.toString());
+    pen.println("getWeight(): " + packageAmazon.getWeight().unit.plural());
+    pen.println("getPrice(): " + packageAmazon.getPrice());
+    pen.println("getName(): " + packageAmazon.getName());
+    pen.println("equals(): " + packageAmazon.equals(packageAmazon));
+    // should be true
+    pen.println();
 
-        /* Merge Test */
-        cart.merge();
-        pen.println("\nAfter Merge:");
-        cart.printContent(pen);
+    /* Cart Test */
+    pen.println("numThings(): " + cart.numThings());
+    pen.println("numEntries(): " + cart.numEntries());
+    cart.printContent(pen);
+    pen.println("getPrice(): " + cart.getPrice());
 
-        /* Rmove Test */
-        cart.remove("amazon");
-        pen.println("\nAfter Deletion: ");
-        cart.printContent(pen);
-    }
+    /* Merge Test */
+    cart.merge();
+    pen.println("\nAfter Merge:");
+    cart.printContent(pen);
+
+    /* Remove Test */
+    cart.remove("amazon");
+    pen.println("\nAfter Deletion: ");
+    cart.printContent(pen);
+  }
 }
